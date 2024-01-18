ed002f642259bd8cdfb73dd9aca19df100c0d9c6
nuwanw <nuwanw@zone24x7.com>
2024-01-18 11:35:01 +0530

    cleaning the main branch

--- src/main/java/com/myapp/designpatterns/DesignPatternsApplication.java
+++ src/main/java/com/myapp/designpatterns/DesignPatternsApplication.java
@@ -1,13 +1,7 @@
 package com.myapp.designpatterns;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DesignPatternsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesignPatternsApplication.class, args);
-	}
+	public static void main(String[] args) {}
 
 }
